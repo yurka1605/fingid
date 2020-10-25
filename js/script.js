f62f13bd2a7c586bb79e3f1eec2de49df044e879
Litvinov Yurka <litvinov.yuriy1996@gmail.com>
2020-10-26 02:04:55 +0400

    feat(mobile): add mobile version on project

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ $('.header__burger').on('click', function () {
 $('.close-btn').on('click', function () {
     $('.mobile__menu').removeClass('active');
 });
+
+$('.banner__point').on('click', function () {
+    let num = 0;
+    $('.banner__point').each((i, el) => {
+        if ($(this).data('num') === $(el).data('num')) {
+            num = i;
+        }
+        $(el).removeClass('active');
+    });
+    $(this).addClass('active');
+    $('.banner__slider').css('transform', `translateX(-${ num * 100 }%)`); 
+});
